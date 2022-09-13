61ae75b75b572936d83867ab06186bf81f014381
Taekeun Hwang <tkhwang@gmail.com>
2022-09-13 10:37:10 +0900

    feat: :sparkles: initial import

---
+++ blog-config.ts
@@ -0,0 +1,22 @@
+const config = {
+  title: 'pushing the boundary', // Blog name
+  author: 'tkhwang', // Your name
+  description: '🔥 One step further towards better than yesterday. ',
+  siteUrl: 'https://tkhwang.me',
+  thumbnail: '/thumbnails/main.png', // Default blog thumbnail. i.e. '/thumbnails/main.png'
+  social: {
+    github: 'tkhwang', // GitHub username
+    twitter: 'techInsightClub', // Twitter username
+    facebook: '', // Facebook username
+    linkedin: '', // LinkedIn username
+    instagram: 'tech_insight_in_public_club', // Instagram username
+  },
+  comment: {
+    utterances: '', // Your GitHub repository
+    disqusShortName: '', // Disqus short name
+  },
+  icon: 'src/images/gatsby-icon.png', // path of blog icon
+  googleAnalytics: '0', // Google Analytics tracking id
+};
+
+export default config;
